6a0b3efb2f0040face4359f34bb719ef0dbe9476
odyseyus <saidusmonovikhrom@gmail.com>
2026-07-18 14:50:52 +0500

    add new chaoters

---
+++ learncpp/01-Basics/mian.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+// int main() 
+// {
+//     std::cout << "C++ is working!" << "\n";
+//     return 0;   
+// }
+
+int main()
+{
+    int player {2};
+    std::cout << player;
+    return 0;
+}
